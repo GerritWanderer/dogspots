b4161437666e44abee53aff05bdef854b87492bf
Gerrit Wanderer <wanderwelten+account@googlemail.com>
2013-01-27 17:39:05 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogspots::Application.config.secret_token = '7be7463f13870c2944ef5e35e263b1ca520a051a2248f5320bc2e205bfe109dfd1e4c9786e7b4660bcf92ee1b75ddbbbe8732f863bec56b4aa28799289698770'
