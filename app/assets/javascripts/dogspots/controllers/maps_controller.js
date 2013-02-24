2ace26b810d7e21107932cb0100eeb7b2098b0a6
Gerrit Wanderer <wanderwelten+account@googlemail.com>
2013-02-24 13:57:28 +0100

    Display InfoWindows for GoogleMapsMarker

--- app/assets/javascripts/dogspots/controllers/maps_controller.js
+++ app/assets/javascripts/dogspots/controllers/maps_controller.js
@@ -1,23 +1,37 @@
 App.MapsController = Ember.ArrayController.extend({
-  content: [],
   i: 0,
-  googleMaps: false,
+  berlin_lat: 52.501476,
+  berlin_lon: 13.402526,
 
   didLoadAllContent: function () {
     if (this.content.everyProperty('isLoaded') && this.content.get('length') > 0) {
-      if (!this.googleMaps) {
-        this.googleMaps = new google.maps.Map(document.getElementById("map_canvas"),  {
-          center: new google.maps.LatLng(52.1244, 13.0454),
-          zoom: 8,
+      if (typeof window.GoogleMaps == "undefined") {
+        window.GoogleMaps = new google.maps.Map(document.getElementById("map_canvas"),  {
+          center: new google.maps.LatLng(this.berlin_lat, this.berlin_lon),
+          zoom: 10,
+          disableDefaultUI: true,
           mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        window.GoogleMapsMarkers = [];
+        window.GoogleMapsInfoWindow = new google.maps.InfoWindow({
+          content: "loading ..."
         });
       }
       if (this.content.get('length') == (this.i+1)) {
-        new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: new google.maps.LatLng(this.content.objectAt(this.i).get('latitude'), this.content.objectAt(this.i).get('longitude')),
-          map: this.googleMaps,
-          title: this.content.objectAt(this.i).get('title')
+          map: window.GoogleMaps,
+          title: this.content.objectAt(this.i).get('title'),
+          infoWindowIndex: this.i
         });
+        window.GoogleMapsMarkers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', function(event) {
+          window.GoogleMaps.panTo(event.latLng);
+          window.GoogleMapsInfoWindow.setContent(this.title);
+          window.GoogleMapsInfoWindow.open(window.GoogleMaps, this);
+        });
+
         this.i++;
       }
     }
